54feabc3f0a20e2164f9d8ad779855b208eb93a5
Quinten Konyn <qkonyn@gmail.com>
2024-12-06 03:38:36 -0800

    Don't parse twice in Day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -2,16 +2,9 @@ use itertools::Itertools;
 use winnow::{ascii::dec_int, combinator::separated, PResult, Parser};
 
 pub fn run(lines: &str) -> (u32, u32) {
-    let part1 = lines
-        .lines()
-        .map(|l| line.parse(l).unwrap())
-        .filter(|l| report(l))
-        .count();
-    let part2 = lines
-        .lines()
-        .map(|l| line.parse(l).unwrap())
-        .filter(|l| dampen(l))
-        .count();
+    let reports: Vec<_> = lines.lines().map(|l| line.parse(l).unwrap()).collect();
+    let part1 = reports.iter().filter(|l| report(l)).count();
+    let part2 = reports.iter().filter(|l| dampen(l)).count();
     (part1 as u32, part2 as u32)
 }
 
