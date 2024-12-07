908680bd11a25a245bc24b1b4f747ec9e7d5d023
Quinten Konyn <qkonyn@gmail.com>
2024-12-07 02:16:32 -0800

    WIP Day3 Part2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,9 +1,9 @@
 use winnow::{
     ascii::dec_uint,
-    combinator::{alt, delimited, preceded, repeat, rest, separated_pair},
+    combinator::{alt, delimited, peek, preceded, repeat, rest, separated_pair},
     stream::AsChar,
     token::{one_of, take_until},
-    PResult, Parser,
+    PResult, Parser, Stateful,
 };
 
 pub fn run(lines: &str) -> (u32, u32) {
@@ -59,6 +59,58 @@ fn parse_muls(input: &mut &str) -> PResult<Vec<(u32, u32)>> {
     Ok(out)
 }
 
+#[derive(Debug)]
+struct State<'s>(&'s mut bool);
+
+impl<'s> State<'s> {
+    fn found_do(&mut self) {
+        *self.0 = true;
+    }
+    fn found_dont(&mut self) {
+        *self.0 = false;
+    }
+}
+
+type Stream<'is> = Stateful<&'is str, State<'is>>;
+
+fn found_do<'s>(i: &mut Stream<'s>) -> PResult<&'s str> {
+    let out = "do()".parse_next(i);
+    // only update the state if the parsing actually succeeds, or else it'll get
+    // messed with when checked during a branch like in alt()
+    if out.is_ok() {
+        i.state.found_do();
+    }
+    out
+}
+
+fn found_dont<'s>(i: &mut Stream<'s>) -> PResult<&'s str> {
+    let out = "don't()".parse_next(i);
+    // only update the state if the parsing actually succeeds, or else it'll get
+    // messed with when checked during a branch like in alt()
+    if out.is_ok() {
+        i.state.found_dont();
+    }
+    out
+}
+
+fn parse_toggled_muls(input: &mut Stream) -> PResult<Vec<(u32, u32)>> {
+    let potentials: Vec<Option<(u32, u32)>> = repeat(0.., potential_functions).parse_next(input)?;
+    rest.parse_next(input)?;
+    let out: Vec<(u32, u32)> = potentials.iter().cloned().flatten().collect();
+    Ok(out)
+}
+
+fn potential_functions(input: &mut Stream) -> PResult<Option<(u32, u32)>> {
+    alt((
+        found_dont.value(None),
+        found_do.value(None),
+        //        mul.map(|p: (u32, u32)| Some(p)),
+        "mul".value(None),
+        //        discard_junk.value(None),
+    ))
+    .parse_next(input)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -73,7 +125,7 @@ mod test {
 
     #[test]
     fn part2() {
-        assert_eq!(run(EXAMPLE).1, todo!());
+        assert_eq!(run(EXAMPLE).1, 48);
     }
 
     #[test]
@@ -125,5 +177,29 @@ mod test {
         let input = "mul(4*mul(44,46)";
         let output = parse_muls.parse(input);
         assert_eq!(output, Ok(vec![(44, 46)]));
+    }
+
+    #[test]
+    fn parse_do() {
+        let input = "do()";
+        let mut state = false;
+        let input = Stream {
+            input,
+            state: State(&mut state),
+        };
+        let output = found_do.parse(input).unwrap();
+        assert_eq!(state, true);
+    }
+
+    #[test]
+    fn parse_failed_do() {
+        let input = "doo()";
+        let mut state = false;
+        let input = Stream {
+            input,
+            state: State(&mut state),
+        };
+        let output = alt((found_do, "doo()")).parse(input).unwrap();
+        assert_eq!(state, false);
     }
 }
