bd1bfc9a8d7929f66fc6bd96d86b606446cb9147
Quinten Konyn <qkonyn@gmail.com>
2024-12-07 05:39:26 -0800

    Trying to get the junk parser to work

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,15 +1,15 @@
 use winnow::{
     ascii::dec_uint,
-    combinator::{alt, delimited, peek, preceded, repeat, rest, separated_pair},
+    combinator::{alt, cond, delimited, peek, preceded, repeat, repeat_till, rest, separated_pair},
     stream::AsChar,
-    token::{one_of, take_until},
+    token::{one_of, take, take_until},
     PResult, Parser, Stateful,
 };
 
 pub fn run(lines: &str) -> (u32, u32) {
-    let mut state = false;
+    let mut state = true;
     let input = Stream {
-        input: lines,
+        input: &lines.to_owned(),
         state: State(&mut state),
     };
     let part1 = parse_muls
@@ -18,11 +18,28 @@ pub fn run(lines: &str) -> (u32, u32) {
         .iter()
         .map(|(a, b)| a * b)
         .sum();
-    (part1, 0)
+
+    let mut state = true;
+    let input = Stream {
+        input: lines,
+        state: State(&mut state),
+    };
+    let part2 = parse_toggled_muls
+        .parse(input)
+        .unwrap()
+        .iter()
+        .map(|(a, b)| a * b)
+        .sum();
+
+    (part1, part2)
 }
 
-fn mul(input: &mut Stream) -> PResult<(u32, u32)> {
-    preceded("mul", args).parse_next(input)
+fn mul(input: &mut Stream) -> PResult<Option<(u32, u32)>> {
+    if *input.state.0 {
+        preceded("mul", args).map(|x| Some(x)).parse_next(input)
+    } else {
+        preceded("mul", args).map(|_| None).parse_next(input)
+    }
 }
 
 fn args(input: &mut Stream) -> PResult<(u32, u32)> {
@@ -45,16 +62,16 @@ fn num(input: &mut Stream) -> PResult<u32> {
 }
 
 fn discard_junk(input: &mut Stream) -> PResult<()> {
-    take_until(0.., "mul").void().parse_next(input)
+    //let mut parser = repeat_till(1.., take(1usize), alt(("do()", "don't()", "mul()"))) //TODO
+    let mut parser = repeat_till(1.., take(1usize), peek("mul"))
+        .map(|((), _)| ())
+        .take()
+        .void();
+    parser.parse_next(input)
 }
 
 fn potential_mul(input: &mut Stream) -> PResult<Option<(u32, u32)>> {
-    alt((
-        mul.map(|p: (u32, u32)| Some(p)),
-        "mul".value(None),
-        discard_junk.value(None),
-    ))
-    .parse_next(input)
+    alt((mul, "mul".value(None), discard_junk.value(None))).parse_next(input)
 }
 
 fn parse_muls(input: &mut Stream) -> PResult<Vec<(u32, u32)>> {
@@ -90,6 +107,8 @@ fn found_do<'s>(i: &mut Stream<'s>) -> PResult<&'s str> {
 
 fn found_dont<'s>(i: &mut Stream<'s>) -> PResult<&'s str> {
     let out = "don't()".parse_next(i);
+    dbg!(&i);
+    dbg!(&out);
     // only update the state if the parsing actually succeeds, or else it'll get
     // messed with when checked during a branch like in alt()
     if out.is_ok() {
@@ -109,8 +128,11 @@ fn potential_functions(input: &mut Stream) -> PResult<Option<(u32, u32)>> {
     alt((
         found_dont.value(None),
         found_do.value(None),
-        mul.map(|p: (u32, u32)| Some(p)),
+        //mul.map(|p: (u32, u32)| Some(p)),
+        mul,
         "mul".value(None),
+        "do()".value(None),
+        "don't()".value(None),
         discard_junk.value(None),
     ))
     .parse_next(input)
@@ -120,35 +142,37 @@ fn potential_functions(input: &mut Stream) -> PResult<Option<(u32, u32)>> {
 mod test {
     use super::*;
 
-    static EXAMPLE: &str =
+    static EXAMPLE1: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    static EXAMPLE2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn part1() {
-        assert_eq!(run(EXAMPLE).0, 161);
+        assert_eq!(run(EXAMPLE1).0, 161);
     }
 
     #[test]
     fn part2() {
-        assert_eq!(run(EXAMPLE).1, 48);
+        assert_eq!(run(EXAMPLE2).1, 48);
     }
 
     #[test]
     fn parse_mul1() {
         let input = "mul(44,46)";
-        let mut state = false;
+        let mut state = true;
         let stream = Stream {
             input,
             state: State(&mut state),
         };
         let output = mul.parse(stream).unwrap();
-        assert_eq!(output, (44, 46));
+        assert_eq!(output, Some((44, 46)));
     }
 
     #[test]
     fn parse_mul2() {
         let input = "mul(1144,46)";
-        let mut state = false;
+        let mut state = true;
         let input = Stream {
             input,
             state: State(&mut state),
@@ -160,7 +184,7 @@ mod test {
     #[test]
     fn parse_digits() {
         let input = "1234";
-        let mut state = false;
+        let mut state = true;
         let input = Stream {
             input,
             state: State(&mut state),
@@ -172,7 +196,7 @@ mod test {
     #[test]
     fn parse_digits_str() {
         let mut input = "1234";
-        let mut state = false;
+        let mut state = true;
         let mut input = Stream {
             input,
             state: State(&mut state),
@@ -184,7 +208,7 @@ mod test {
     #[test]
     fn parse_ignore_invalid() {
         let input = "nul(4*mul(44,46)";
-        let mut state = false;
+        let mut state = true;
         let mut stream = Stream {
             input,
             state: State(&mut state),
@@ -197,7 +221,7 @@ mod test {
     #[test]
     fn parse_skip_invalid_mul() {
         let input = "mul(4*mul(44,46)";
-        let mut state = false;
+        let mut state = true;
         let mut stream = Stream {
             input,
             state: State(&mut state),
@@ -210,7 +234,7 @@ mod test {
     #[test]
     fn parse_multiple_mul() {
         let input = "mul(4*mul(44,46)";
-        let mut state = false;
+        let mut state = true;
         let input = Stream {
             input,
             state: State(&mut state),
@@ -241,5 +265,10 @@ mod test {
         };
         let output = alt((found_do, "doo()")).parse(input).unwrap();
         assert_eq!(state, false);
+    }
+
+    #[test]
+    fn part2_partial() {
+        assert_eq!(run(EXAMPLE2).1, 48);
     }
 }
