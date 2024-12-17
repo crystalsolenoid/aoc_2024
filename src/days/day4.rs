14f2b0865af2f61080f548a9b513500ea42b1e5d
Quinten Konyn <qkonyn@gmail.com>
2024-12-16 23:27:24 -0800

    Iterate over diagonals down and to the right

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,11 +1,12 @@
 use grid::Grid;
 use itertools::Itertools;
-use std::iter::StepBy;
+use std::iter::{Flatten, StepBy, Take};
 
 const WORD: [u8; 4] = [b'X', b'M', b'A', b'S'];
 
 pub fn run(lines: &str) -> (u32, u32) {
     let grid: Vec<u8> = lines.lines().flat_map(|l| l.as_bytes().to_vec()).collect();
+    let grid: Vec<u8> = (0..100).collect();
     let width = lines.lines().next().unwrap().len();
     let grid = Grid::from_vec(grid, width);
     dbg!(&WORD);
@@ -16,6 +17,8 @@ pub fn run(lines: &str) -> (u32, u32) {
         .flat_map(|row| SearchIter::new(row, &WORD))
         .filter(|x| *x)
         .count());
+    let diagonal2: Vec<_> = diagonal_right(&grid, 2).collect();
+    dbg!(diagonal2);
     let part1 = 0;
     let part2 = 0;
     (part1 as u32, part2 as u32)
@@ -23,6 +26,14 @@ pub fn run(lines: &str) -> (u32, u32) {
 
 fn rows(grid: &Grid<u8>) -> usize {
     grid.iter_col(1).count()
+}
+
+fn diagonal_right<'a, T>(grid: &'a Grid<T>, x: usize) -> Take<StepBy<std::slice::Iter<'a, T>>> {
+    // TODO this only works with the default row-major order right now...
+    assert!((0..grid.cols()).contains(&x));
+    let mut diag = grid.iter();
+    let _ = diag.nth(x - 1);
+    diag.step_by(grid.cols() + 1).take(grid.cols() - x)
 }
 
 struct SearchIter<'a> {
